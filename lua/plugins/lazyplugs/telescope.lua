8750d7415329e70532e1d3cb9fb0430fd282c4a5
Daniel Aviv <dani6laviv@gmail.com>
2024-04-21 20:15:51 +0300

    added lsp shortcut keys

--- lua/plugins/lazyplugs/telescope.lua
+++ lua/plugins/lazyplugs/telescope.lua
@@ -30,13 +30,24 @@ return {
                 find_buffers = {
                     theme = "dropdown",
                     previewer = false,
+                },
+                help_tags = {
+                    theme = "dropdown",
+                    previewer = false,
                 }
             }
         }
         require("telescope").setup(settings)
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
+        vim.keymap.set("n", "<leader>pf", builtin.find_files, {desc = "[P]roject [F]iles"})
+        vim.keymap.set("n", "<leader>pg", builtin.live_grep, {desc = "[P]roject [G]rep"})
+        vim.keymap.set("n", "<leader>pb", builtin.buffers, {desc = "[P]roject [B]uffers"})
+        vim.keymap.set("n", "<leader>ht", builtin.help_tags, {desc = "[H]elp [T]ags"})
+        vim.keymap.set("n", "<leader>lgT", builtin.lsp_type_definitions, {desc = "[L]SP [G]oto [T]ype definitions"})
+        vim.keymap.set("n", "<leader>lgd", builtin.lsp_definitions, {desc = "[L]SP [G]oto [D]efinitions"})
+        vim.keymap.set("n", "<leader>lgr", builtin.lsp_references, {desc = "[L]SP [G]oto [R]eferences"})
+        vim.keymap.set("n", "<leader>lgI", builtin.lsp_implementations, {desc = "[L]SP [G]oto [I]mplementations"})
+        vim.keymap.set("n", "<leader>lds", builtin.lsp_document_symbols, { desc = "[L]SP [D]ocument [S]ymbols" })
+        vim.keymap.set("n", "<leader>lws", builtin.lsp_dynamic_workspace_symbols, { desc = "[L]SP [W]orkspace [S]ymbols" })
     end
 }
--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -14,7 +14,11 @@ return {
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local opts = {noremap = true, silent = true}
         local on_attach = function(client, bufnr)
-            -- TODO: set keymaps!
+            vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "LSP: ".."[R]e[n]ame" })
+            vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, { desc = "[L]SP [R]ename" })
+            vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "LSP: ".."[G]oto [D]eclaration" })
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: ".."[C]ode [A]ction" })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: ".."Hover Documentation" })
         end
         local capabilities = cmp_nvim_lsp.default_capabilities()
         local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
