3c6d6215ccf8cc702628f84345f0606fd1aec53d
Daniel Aviv <dani6laviv@gmail.com>
2024-05-01 15:32:07 +0300

    changed theme to rosepine

--- lua/plugins/lazyplugs/gruvbox.lua
+++ lua/plugins/lazyplugs/gruvbox.lua
@@ -1,5 +1,6 @@
 return {
     "ellisonleao/gruvbox.nvim",
+    enabled = false,
     name = "gruvbox",
     priority = 1000,
     config = function()
---
+++ lua/plugins/lazyplugs/rosepine.lua
@@ -0,0 +1,16 @@
+return {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    config = function()
+        require("rose-pine").setup({
+            disable_background = true,
+            styles = {
+                italic = false,
+            },
+        })
+        vim.cmd.colorscheme "rose-pine"
+        vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+        vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+    end,
+}
